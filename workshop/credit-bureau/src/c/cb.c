2d49ab6490d3ff62c3b8f329e1940be608082fc1
Miguel A.Vizcaino G <mvizkg@gmail.com>
2013-06-12 14:26:06 -0500

    avance miercoles

--- workshop/credit-bureau/src/c/cb.c
+++ workshop/credit-bureau/src/c/cb.c
@@ -10,35 +10,106 @@
 #include <sys/types.h>
 #include <winsock2.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define PORT 3550 /* El puerto que será abierto */
 #define BACKLOG 2 /* El número de conexiones permitidas */
 
-void doprocessing (int sock)
+void selectionMethod( int sock, char *rfc, char i )
 {
-    int n;
+	switch( i ) 
+	{
+		case 's':
+			searchRFC( sock, rfc );
+		case 'c' :
+			break;
+		case 'a' :
+			break;
+	}
+}
+
+void searchRFC( int sock, char *rfc )
+{
+	int n, x;
+	x = strlen(rfc);
+	
+	printf( "\nRFC:%s", rfc );
+	printf( "\nLength: %d", x );
+	
+	FILE *f;
+	f = fopen("Loans.txt", "r");
+	char line[100];
+	char word[x];
+	char *p;
+	bool found = false;
+
+	while( fgets( line, 100, f ) != NULL )
+	{
+		p = line + 4;
+		
+		strncpy( word, p, 10 );
+		//printf( "\nword:%s", word );
+		
+		if( strncmp( word, rfc, 10 ) == 0 )
+		{
+			send( sock, line, strlen(line), 0 );
+			printf("\nLine:%sLength:%d", line, strlen(line) );
+			found = true;
+		}
+    }
+	if( found == false )
+	{
+		send( sock, rfc, x, 0 );
+		send( sock, "\t>>  Not Found!", 17, 0 );
+		printf("\n%s Not Found.", rfc);
+	}
+	for( n = 0; n < 10; n ++)
+	{
+		word[n] = '0';
+	}
+	p = '\0';
+	fclose( f );
+}
+
+void doprocessing( int sock )
+{
+    int x;
     char buffer[256];
 
-    memset(&(buffer), '0', 256);
+    memset( &(buffer), '0', 256 );
     int recvMsgSize;
-    
+	recvMsgSize = recv( sock, buffer, 256, 0 );
+	char rfc[recvMsgSize -1];
+	
+	for( x = 0; x < recvMsgSize -1; x++)
+	{
+		rfc[x] = buffer[x+1];
+		//buffer[x] = '0';
+	}
+	//buffer[recvMsgSize -1] = '0';
+	
     /* Receive message from client */
-    if ((recvMsgSize = recv(sock, buffer, 256, 0)) < 0)
-        perror("ERROR reading to socket");
+    /* if( ( recvMsgSize = recv(sock, buffer, 256, 0) ) < 0 )
+	{
+        perror("*ERROR reading to socket");
+	} */
 
-    /* Send received string and receive again until end of transmission */
-    while (recvMsgSize > 0)      /* zero indicates end of transmission */
-    {
-        /* Echo message back to client */
-        if (send(sock, buffer, recvMsgSize, 0) != recvMsgSize)
-            perror("ERROR writing to socket");
+	/* Send received string and receive again until end of transmission */
+	while( recvMsgSize > 0 )      // zero indicates end of transmission
+	{
+		selectionMethod( sock, rfc, buffer[0] );
+			
+		/* Echo message back to client */
+		if( send( sock, buffer, recvMsgSize, 0 ) != recvMsgSize ) {
+			perror("\nERROR writing to socket");
+		}
 
-        /* See if there is more data to receive */
-        if ((recvMsgSize = recv(sock, buffer, 256, 0)) < 0)
-            perror("ERROR reading to socket");
-    }
-
-    closesocket(sock);    /* Close client socket */
+		/* See if there is more data to receive */
+		if( ( recvMsgSize = recv( sock, buffer, 256, 0 ) ) < 0 ) {
+			perror("\nERROR reading to socket");
+		}
+	}
+	closesocket(sock);    /* Close client socket */
 }
 
 BOOL initW32() 
@@ -70,66 +141,66 @@ BOOL initW32()
 
 int main()
 {
-
-	 initW32(); /* Necesaria para compilar en Windows */ 
+	initW32(); /* Necesaria para compilar en Windows */ 
 	 	
-   int fd, fd2; /* los descriptores de archivos */
+	int fd, fd2; /* los descriptores de archivos */
 
-   /* para la información de la dirección del servidor */
-   struct sockaddr_in server;
+	/* para la información de la dirección del servidor */
+	struct sockaddr_in server;
 
-   /* para la información de la dirección del cliente */
-   struct sockaddr_in client;
+	/* para la información de la dirección del cliente */
+	struct sockaddr_in client;
 
-   unsigned int sin_size;
+	unsigned int sin_size;
 
-   pid_t pid;
+	pid_t pid;
 
-   /* A continuación la llamada a socket() */
-   if ((fd=socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
-      printf("error en socket()\n");
+	/* A continuación la llamada a socket() */
+	if( ( fd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 ) {
+      printf("Error en socket()\n");
       exit(-1);
-   }
+	}
 
-   server.sin_family = AF_INET;
+	server.sin_family = AF_INET;
 
-   server.sin_port = htons(PORT);
+	server.sin_port = htons(PORT);
 
-   server.sin_addr.s_addr = INADDR_ANY;
-   /* INADDR_ANY coloca nuestra dirección IP automáticamente */
+	server.sin_addr.s_addr = INADDR_ANY;
+	/* INADDR_ANY coloca nuestra dirección IP automáticamente */
 
-   //bzero(&(server.sin_zero),8);
-   memset(&(server.sin_zero), '0', 8);
-   /* escribimos ceros en el reto de la estructura */
+	//bzero(&(server.sin_zero),8);
+	memset( &(server.sin_zero), '0', 8 );
+	/* escribimos ceros en el reto de la estructura */
 
 
-   /* A continuación la llamada a bind() */
-   if(bind(fd,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1) {
-      printf("error en bind() \n");
-      exit(-1);
-   }
+	/* A continuación la llamada a bind() */
+	if( bind( fd,(struct sockaddr*) &server, sizeof( struct sockaddr ) ) == -1 ) {
+		printf("error en bind() \n");
+		exit(-1);
+	}
 
-   if(listen(fd,BACKLOG) == -1) {  /* llamada a listen() */
+   if( listen(fd, BACKLOG) == -1 ) {  /* llamada a listen() */
       printf("error en listen()\n");
       exit(-1);
    }
 
-   while(1) {
-      sin_size=sizeof(struct sockaddr_in);
-      /* A continuación la llamada a accept() */
-      if ((fd2 = accept(fd,(struct sockaddr *)&client, &sin_size))==-1) {
+   while(1)
+   {
+      sin_size = sizeof( struct sockaddr_in );
+      
+	  /* A continuación la llamada a accept() */
+      if( ( fd2 = accept( fd, (struct sockaddr *) &client, &sin_size )) == -1 ) {
          printf("error en accept()\n");
          exit(-1);
       }
 
-      printf("Se obtuvo una conexión desde %s\n", inet_ntoa(client.sin_addr) );
+      printf("Se obtuvo una conexion desde %s\n", inet_ntoa( client.sin_addr ) );
       /* que mostrará la IP del cliente */
 
-      send(fd2,"Bienvenido a mi servidor.\n",22,0);
+      send( fd2, "Hola, bienvenido Miguel Angel\n", 32, 0 );
       /* que enviará el mensaje de bienvenida al cliente */
       
       doprocessing(fd2);
 
    } /* end while */
 }
-
